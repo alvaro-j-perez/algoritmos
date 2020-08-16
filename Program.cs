12de5629f8e768dff16454b303616b02b924dfe7
alvaro-j-perez <aperezg55@miumg.edu.gt>
2020-08-15 19:04:08 -0600

    mi primer programa en C#

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace semana_3_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+                int num = 10;
+            double result = Math.Sqrt(num);
+            Console.WriteLine(result);
+            Console.Write($"es la raiz cuadrada de su numero" );
+            
+            }   catch(Exception ex){
+                Console.Write(ex.Message);
+
+            }Console.ReadKey();
+        }
+    }
+}
+
